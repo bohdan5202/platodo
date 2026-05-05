bfbf64831473b9252b07426cef719cb4089e6b43
bohdan5202 <kramar24155@stud.akademiabialska.pl>
2026-05-05 03:32:22 +0200

    Last try

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse, type NextRequest } from 'next/server';
 
-export const runtime = 'edge';
+export const runtime = 'experimental-edge';
 const PUBLIC_ROUTES = ['/login', '/register', '/forgot-password', '/reset-password'];
 
 export function middleware(request: NextRequest) {
